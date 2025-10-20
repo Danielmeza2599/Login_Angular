10a8b1b48d4495f3fd76ca21af4487c5a6b5190b
Danielmeza2599 <69852644+Danielmeza2599@users.noreply.github.com>
2025-10-20 13:06:21 -0600

    Navbar 03 added and login

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Navbar03 } from './layouts/navbars/navbar03/navbar03';
+import { Login } from './auth/login/login';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Navbar03,
+    Login
   ],
   imports: [
     BrowserModule,
---
+++ src/app/layouts/navbars/navbar03/navbar03.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar03',
+  standalone: false,
+  templateUrl: './navbar03.html',
+  styleUrl: './navbar03.scss'
+})
+export class Navbar03 {
+
+}
